2cb77bca701ea21ababda7b294b8921164352cbf
souiti nakajima <nakajimasouiti@gmail.com>
2018-09-16 09:24:47 +0000

    kadai2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @microposts = @user.like_microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+    render :show
+  end
+  
    private
 
   def user_params
